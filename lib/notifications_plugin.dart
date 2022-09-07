9e0a28f13ef60d5662a363180cab75f95536662c
tupt <tupt@rikkeisoft.com>
2022-09-07 14:01:43 +0700

    done push notification

--- lib/notifications_plugin.dart
+++ lib/notifications_plugin.dart
@@ -1,37 +1,78 @@
-import 'package:firebase_messaging/firebase_messaging.dart';
+import 'dart:convert';
+
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class NotificationPlugin {
-  FirebaseMessaging messaging = FirebaseMessaging.instance;
-  final AndroidNotification _androidNotification = const AndroidNotification(
-      channelId: "high_importance_channel",
-      priority: AndroidNotificationPriority.highPriority);
+  final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+  final FirebaseMessaging _messaging = FirebaseMessaging.instance;
+  final AndroidNotificationChannel _channel = const AndroidNotificationChannel(
+      'high_importance_channel', 'High Importance Notifications',
+      description: 'This channel is used for important notifications.',
+      importance: Importance.max,
+      playSound: true);
 
   NotificationPlugin() {
     Firebase.initializeApp();
-    setupPermission();
-    setUpFireBaseNotification();
+    _requestPermission();
+    _createChannel();
+    _getToken();
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print("message ${message.toMap().toString()}");
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+      var initializationSettingsAndroid =
+          new AndroidInitializationSettings('@mipmap/ic_launcher');
+      if (notification != null && android != null) {
+        _flutterLocalNotificationsPlugin.show(
+          DateTime.now().millisecond,
+          message.notification?.title,
+          message.notification?.body,
+          NotificationDetails(
+            android: AndroidNotificationDetails(
+              _channel.id,
+              _channel.name,
+              priority: Priority.max,
+              importance: Importance.max,
+              icon: "@mipmap/ic_launcher",
+              enableVibration: true,
+            ),
+          ),
+          payload: json.encode(message.data),
+        );
+      }
+    });
   }
 
-  void setupPermission() async {
-    NotificationSettings settings = await messaging.requestPermission(
+  Future<void> _createChannel() async {
+    await _flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(_channel);
+  }
+
+  Future<void> _getToken() async {
+    String? token = await _messaging.getToken();
+    print("Token $token");
+  }
+
+  Future<void> _requestPermission() async {
+    await _messaging.requestPermission(
       alert: true,
       announcement: false,
       badge: true,
       carPlay: false,
-      criticalAlert: false,
+      criticalAlert: true,
       provisional: false,
       sound: true,
     );
-  }
 
-  void setUpFireBaseNotification() async {
-    String? token = await messaging.getToken();
-    print("token $token");
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      print('Got a message whilst in the foreground!');
-      print('Message data: ${message.toMap().toString()}');
-    });
+    await _messaging.setForegroundNotificationPresentationOptions(
+      alert: true, // Required to display a heads up notification
+      badge: true,
+      sound: true,
+    );
   }
 }
