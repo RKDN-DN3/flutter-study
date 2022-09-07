652221ded3367f72541e1ef8e67ef4c6c00679c9
tupt <tupt@rikkeisoft.com>
2022-09-07 10:06:44 +0700

    add notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:example_flutter/container/auth/auth_provider.dart';
+import 'package:example_flutter/notifications_plugin.dart';
 import 'package:example_flutter/routers/root_router.dart';
 import 'package:example_flutter/container/home/home_provider.dart';
 import 'package:example_flutter/utils/screen.dart';
@@ -6,8 +7,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 import 'generated/l10n.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     /// Providers are above [MyApp] instead of inside it, so that tests
     /// can use [MyApp] while mocking the providers
@@ -21,8 +29,20 @@ void main() {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _App();
+}
+
+class _App extends State<MyApp> {
+  @override
+  void initState() {
+    NotificationPlugin();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
